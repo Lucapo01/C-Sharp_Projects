51d5b90a6ec26932c37ef4e7d16ca3203aeac42c
Luciano Garbino <lucigar01@gmail.com>
2019-06-27 20:08:58 -0300

    27-06-19

--- Sentencias2/Program.cs
+++ Sentencias2/Program.cs
@@ -6,7 +6,59 @@ namespace Sentencias2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            for (int i = 0; i < 10; i++)
+            {
+                
+            
+            
+            string fruta;
+
+
+
+            Console.WriteLine("Ingrese su fruta: ");
+
+
+            fruta = Console.ReadLine();
+
+
+
+            switch (fruta.ToLower())
+            {
+
+                case "naranja":
+
+                    Console.WriteLine("Hay juguito");
+
+                    break;
+
+                case "manzana":
+
+                    Console.WriteLine("Hay tartita");
+
+                    break;
+
+                case "pomelo":
+
+                    Console.WriteLine("Hay termita con pomelo");
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Se lo tiro por la ventana a un transeunte pseudo comunista");
+
+                    break;
+
+                   
+
+                    
+
+
+            }
+
+            }
         }
     }
 }
